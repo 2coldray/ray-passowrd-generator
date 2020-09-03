22714f5c8b4ea14e8d2f5d9b81fbf2dd71f2ccf0
Ray Bray <rbray.bray2@gmail.com>
2020-09-03 00:27:44 -0400

    added four new variables which are strings of all the characters

--- Develop/script.js
+++ Develop/script.js
@@ -14,28 +14,34 @@ function writePassword() {
 function generatePassword() {
   //make var for user prompt
   var userPrompt = prompt("How many characters would you like your password to be?");
-  console.log(userPrompt)
+  console.log(userPrompt);
 
   //Need if/else statement for telling user to pick number between lower case
   if (userPrompt < 8 || userPrompt > 128) {
-    alert("Number must be between 8 and 128")
+    alert("Number must be between 8 and 128");
   }
 
   //Make variable called upperCased letters
-  var upperCased = confirm("Do you want uppercase letters?")
-  console.log(upperCased)
+  var upperCased = confirm("Click OK to confirm upper case letters");
+  console.log(upperCased);
 
   //Make variable called lowerCased letters
-  var lowerCased = confirm("Do you want lowercse letters?")
-  console.log(lowerCased)
+  var lowerCased = confirm("Click OK to confirm lower case letters");
+  console.log(lowerCased);
 
   //Make variable called numbers
-  var numCharacters = confirm("Do you want numeric characters?")
-  console.log(numCharacters)
+  var numCharacters = confirm("Click OK to confirm numeric characters");
+  console.log(numCharacters);
 
   //Make variable called special characters
-  var specialCharacters = confirm("Do you want special characters?")
-  console.log(specialCharacters)
+  var specialCharacters = confirm("Click OK to confirm special characters");
+  console.log(specialCharacters);
+
+  //Make array with just lowercase, uppercase, numbers, and special characters
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "1234567890"
+  var specialCharacter = "!@#$%^&*()_+{}[]\?><,.~"
 
 }
 
