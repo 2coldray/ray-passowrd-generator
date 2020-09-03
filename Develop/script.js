8f3940ef2213dcee416efe20f0ea231d4b1e71df
Ray Bray <rbray.bray2@gmail.com>
2020-09-03 01:14:01 -0400

    made if else statements to push string elements to new string based on users reponses

--- Develop/script.js
+++ Develop/script.js
@@ -7,13 +7,14 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 //Define Function generatePassword();
 function generatePassword() {
   //make var for user prompt
-  var userPrompt = prompt("How many characters would you like your password to be?");
+  var userPrompt = prompt(
+    "How many characters would you like your password to be?"
+  );
   console.log(userPrompt);
 
   //Need if/else statement for telling user to pick number between lower case
@@ -40,9 +41,29 @@ function generatePassword() {
   //Make array with just lowercase, uppercase, numbers, and special characters
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbers = "1234567890"
-  var specialCharacter = "!@#$%^&*()_+{}[]\?><,.~"
+  var numbers = "1234567890";
+  var specialCharacter = "!@#$%^&*()_+{}[]?><,.~";
 
+  //Make an empty string that holds random characters given the users prompt response
+  var charAvailable = "";
+
+  //Make if === true statements for each case, then add to string
+  if (upperCased === true) {
+    charAvailable = charAvailable.concat(upperCased);
+  } else if (lowerCased === true) {
+    charAvailable = charAvailable.concat(lowerCased);
+  } else if (numbers === true) {
+    charAvailable = charAvailable.concat(numbers);
+  } else if (specialCharacters === true) {
+    charAvailable = charAvailable.concat(specialCharacters);
+  } else {
+    alert("Must choose at least one character type");
+  }
+
+  //Make Variable called Result
+  var finalResult = "";
+
+  //Make for loop to loop over charAvailable, use length of userPrompt
 }
 
 // Add event listener to generate button
